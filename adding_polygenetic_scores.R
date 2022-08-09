588899892b312ca8d7fec754be40c3d6df7561c6
AliyaAM <aliya.amirova005@gmail.com>
2022-08-09 22:10:11 +0100

    updated

--- adding_polygenetic_scores.R
+++ adding_polygenetic_scores.R
@@ -1,36 +1,7 @@
 library(dplyr)
 
-directory = "/Users/aliyaamirova/Documents/KCL_postDoc/"
-
-SOURCE_ROOT = "Data_analysis/"
-OUTPUT_ROOT = "Data_analysis/"
 
 
-HRS_2008_data = read.csv(paste(directory, SOURCE_ROOT, "HRS_2008_data/HRS2008_dataset_latest.csv", sep=""))
-HRS_2010_data = read.csv(paste(directory, SOURCE_ROOT, "HRS_2010_data/HRS2010_dataset_latest.csv", sep=""))
-HRS_2012_data =  read.csv(paste(directory, SOURCE_ROOT, "HRS_2012_data/HRS2012_dataset_latest.csv", sep=""))
-HRS_2014_data =  read.csv(paste(directory, SOURCE_ROOT, "HRS_2014_data/HRS2014_dataset_latest.csv", sep=""))
-HRS_2016_data =  read.csv(paste(directory, SOURCE_ROOT, "HRS_2016_data/HRS2016_dataset_latest.csv", sep=""))
-HRS_2018_data =  read.csv(paste(directory, SOURCE_ROOT, "HRS_2018_data/HRS2018_dataset_latest.csv", sep=""))
-
-
-ELSA_data = read.csv(paste(directory, SOURCE_ROOT, "DATA_ELSA/ELSAdiscrimination_data_wave5.csv", sep = ""))
-
-#HRS polygenic scores data: 
-polygenic_scores_data = read.csv(paste(directory, SOURCE_ROOT, "HRS_2012_data/pgenscore4e_r.csv", sep = ""))
-
-#ELSA polygenic scores data: 
-polygenic_scores_ELSA_data = read.csv(paste(directory, SOURCE_ROOT, "DATA_ELSA/polygenic_scores_ELSA_data_standardized.csv", sep = "")) 
-
-
-ID_ELSA = unique(polygenic_scores_ELSA_data$idauniq)
-
-#polygenic_scores_data$HHIDPN = subset(polygenic_scores_data, polygenic_scores_data$HHIDPN == c(ID))
-
-#HRS_2008_data_with_PGS = subset(HRS_2008_data, HRS_2008_data$)
-
-ELSA_data_polygenic_scores = polygenic_scores_ELSA_data[ELSA_data$idauniq %in% ID_ELSA,]
-ELSA_data_with_PGS = left_join(ELSA_data, ELSA_data_polygenic_scores) 
 
 
 #need to add hypertension_bin to 2018 
@@ -44,37 +15,67 @@ ELSA_data_with_PGS = left_join(ELSA_data, ELSA_data_polygenic_scores)
 #add kidney disease for all years    
 #limiting longstanding condition (recode to bin)
 
-HRS_2008_data$HHIDPN
-HRS_2008_data$HRS2008_emo_psychiat_prob_ever
-HRS_2008_data$HRS2008_emo_psychiat_prob_bin
-
-HRS_2008_data$HRS2008_checklist_depression_bin
-unique(HRS_2008_data$smokes_now_bin)
 
 
 
-polygenic_scores_data$E4_ADHD_PGC17
+directory = "/Users/aliyaamirova/Documents/KCL_postDoc/"
 
-#check minimum age: 
+SOURCE_ROOT = "Data_analysis/"
+OUTPUT_ROOT = "Data_analysis/"
+
+HRS_2008_data = read.csv(paste(directory, SOURCE_ROOT, "HRS_2008_data/HRS2008_dataset_latest.csv", sep=""))
+HRS_2010_data = read.csv(paste(directory, SOURCE_ROOT, "HRS_2010_data/HRS2010_dataset_latest.csv", sep=""))
+HRS_2012_data =  read.csv(paste(directory, SOURCE_ROOT, "HRS_2012_data/HRS2012_dataset_latest.csv", sep=""))
+HRS_2014_data =  read.csv(paste(directory, SOURCE_ROOT, "HRS_2014_data/HRS2014_dataset_latest.csv", sep=""))
+HRS_2016_data =  read.csv(paste(directory, SOURCE_ROOT, "HRS_2016_data/HRS2016_dataset_latest.csv", sep=""))
+HRS_2018_data =  read.csv(paste(directory, SOURCE_ROOT, "HRS_2018_data/HRS2018_dataset_latest.csv", sep=""))
+
+vars_names_2016 =ls(HRS_2016_data)
+
+write.csv(vars_names_2016, file = paste(directory, SOURCE_ROOT, "HRS_2016_data/HRS2016_vars_names.csv", sep=""))
+
+ELSA_data = read.csv(paste(directory, SOURCE_ROOT, "DATA_ELSA/ELSAdiscrimination_data_wave5.csv", sep = ""))
+
+#HRS polygenic scores data: 
+polygenic_scores_data = read.csv(paste(directory, SOURCE_ROOT, "HRS_2012_data/pgenscore4e_r.csv", sep = ""))
+
+#ELSA polygenic scores data: 
+polygenic_scores_ELSA_data = read.csv(paste(directory, SOURCE_ROOT, "DATA_ELSA/polygenic_scores_ELSA_data_standardized.csv", sep = "")) 
+ID_ELSA = unique(polygenic_scores_ELSA_data$idauniq)
+
+ELSA_data_polygenic_scores = polygenic_scores_ELSA_data[ELSA_data$idauniq %in% ID_ELSA,]
+ELSA_data_with_PGS = left_join(ELSA_data, ELSA_data_polygenic_scores) 
+
+
+
+#check minimum age is above 50: 
 
 print(min(HRS_2008_data$continious_age))
-
 print(min(HRS_2010_data$continious_age))
-
 print(min(HRS_2012_data$continious_age))
-
 print(min(HRS_2014_data$continious_age))
-
 print(min(HRS_2016_data$continious_age))
-
 print(min(HRS_2018_data$continious_age))
 
+#ELSA_data_with_PGS$age (check min age is above 50 in ELSA)
+print(min(ELSA_data_with_PGS$w8age, na.rm = TRUE))
 
-#HRS_2008_data$HHIDPN example: 16870011
-#polygenic_scores_data$HHID example: 16700
-#polygenic_scores_data$PN example: 11
+#subset the data to those aged 50 and over: 
 
-#check how HRS HHIDPN were coded 
+ELSA_data_with_PGS = subset(ELSA_data_with_PGS, 
+                            ELSA_data_with_PGS$w1age >=50 & 
+                            ELSA_data_with_PGS$w2age >=50 & 
+                            ELSA_data_with_PGS$w3age >=50 & 
+                            ELSA_data_with_PGS$w4age >=50 & 
+                            ELSA_data_with_PGS$w5age >=50 & 
+                            ELSA_data_with_PGS$w6age >=50 & 
+                            ELSA_data_with_PGS$w7age >=50 & 
+                            ELSA_data_with_PGS$w8age >=50) 
+
+
+
+#check how HRS HHIDPN were coded, rame the IDs so they match the HRS dataset
+
 polygenic_scores_data$HHIDPN = paste(polygenic_scores_data$HHID, 0, polygenic_scores_data$PN, sep = "")
 polygenic_scores_data$HHIDPN = as.numeric(polygenic_scores_data$HHIDPN)
 
@@ -88,9 +89,7 @@ all_cases = c(HRS_2008_data$HHIDPN,
 
 ID = unique(polygenic_scores_data$HHIDPN)
 
-#polygenic_scores_data$HHIDPN = subset(polygenic_scores_data, polygenic_scores_data$HHIDPN == c(ID))
 
-#HRS_2008_data_with_PGS = subset(HRS_2008_data, HRS_2008_data$)
 
 HRS_2008_data_polygenic_scores = polygenic_scores_data[HRS_2008_data$HHIDPN %in% ID,]
 HRS_2010_data_polygenic_scores = polygenic_scores_data[HRS_2010_data$HHIDPN %in% ID,]
@@ -108,11 +107,10 @@ all_polygenic_scores = rbind(HRS_2008_data_polygenic_scores,
                                HRS_2016_data_polygenic_scores, 
                                HRS_2018_data_polygenic_scores) 
 
-#154 people out of those who took part in the HRS study between 2008 and 2018 provided their DNA sample 
+
 people_with_PGS = unique(all_polygenic_scores$HHIDPN)
 
-# add these 154 individual PGS to appropriate dataframes 
-
+# add  PGSs to appropriate dataframes 
 
 HRS_2008_data_with_PGS = left_join(HRS_2008_data, HRS_2008_data_polygenic_scores) 
 HRS_2010_data_with_PGS = left_join(HRS_2010_data, HRS_2010_data_polygenic_scores) 
@@ -121,7 +119,6 @@ HRS_2014_data_with_PGS = left_join(HRS_2014_data, HRS_2014_data_polygenic_scores
 HRS_2016_data_with_PGS = left_join(HRS_2016_data, HRS_2016_data_polygenic_scores) 
 HRS_2018_data_with_PGS = left_join(HRS_2018_data, HRS_2018_data_polygenic_scores) 
 
-
 nrow(HRS_2008_data_with_PGS)
 nrow(HRS_2010_data_with_PGS)
 nrow(HRS_2012_data_with_PGS)
@@ -129,167 +126,3 @@ nrow(HRS_2014_data_with_PGS)
 nrow(HRS_2016_data_with_PGS)
 nrow(HRS_2018_data_with_PGS)
 
-
-nrow(all_polygenic_scores)
-
-unique(all_polygenic_scores$HHIDPN)
-
-#ageism
-HRS_2008_data_with_PGS$reason_discrim1_reason_age
-
-
-#restrict to race 
-HRS_2008_data_with_PGS$race_white
-
-#recode across the years so it is called the same (NO PGS) 
-HRS_2008_data_with_PGS$HRS2008_cancer_bin
-
-
-
-########################### 
-
-
-#recode so it is bin var for diabetes 
-HRS_2008_data_with_PGS$diabetes_new
-
-#E4_T2D_DIAGRAM12 (PGS) 
-# European Ancestry: Type 2 diabetes (T2D) Polygenic Score (DIAGRAM 2012). The
-# PGSs for Type II Diabetes (T2D) were created using GWAS meta-analysis results
-# from a 2012 study conducted by the DIAbetes Genetics Replication and
-# Meta-analysis (DIAGRAM) Consortium.
-#E4_T2D_DIAGRAM12 (PGS: T2DM) 
-HRS_2008_data_with_PGS$E4_T2D_DIAGRAM12
-
-########################### 
-
-
-#coranary heart disease (no separate question on coronary heart disease)
-#Did a doctor ever tell [FIRST NAME] that [he/she] had a heart attack, coronary heart disease, angina, congestive heart failure, or other heart problems?
-HRS_2008_data_with_PGS$heartcondition_bin
-
-
-#add new
-HRS_2008_data_with_PGS$heartcondition_new_bin
-
-#E4_CD_CARDIOGRAM11 (PGS: CAD)
-HRS_2008_data_with_PGS$E4_CD_CARDIOGRAM11
-
-########################### 
-
-#stroke (NO PGS)
-HRS_2008_data_with_PGS$stroke_new_bin
-
-
-########################### 
-
-#chronic lung disease  (NO PGS)
-HRS_2008_data_with_PGS$HRS2008_lungdisease_bin
-#add new (recode to bin)
-HRS_2008_data_with_PGS$HRS2008_lungdisease_new
-
-
-########################### 
-
-#arthritis 
-#add new arthritis (NO PGS) 
-HRS_2008_data_with_PGS$HRS2008_arthritis_bin
-#add new   (recode to bin)
-HRS_2008_data_with_PGS$HRS2008_arthritis_new
-
-########################### 
-
-#limiting longstanding condition 
-HRS_2008_data_with_PGS$limiting_condition_bin
-#add new   (recode to bin)
-
-########################### 
-
-#depression 
-HRS_2008_data_with_PGS$checklist_depression_bin
-
-#E4_DEPSYMP_SSGAC16 depressive symptoms (PGS) 
-HRS_2008_data_with_PGS$E4_DEPSYMP_SSGAC16
-
-#E4_MDD_PGC13 (major depressive disorder  2013 study)
-HRS_2008_data_with_PGS$E4_MDD_PGC13
-
-#E4_MDD2_PGC18 (najor depressive disorder 2018 study)
-HRS_2008_data_with_PGS$E4_MDD2_PGC18
-
-########################### 
-
-#anxiety (ADD) 
-
-#E4_ANXFS_ANGST16              EA ANXIETY FACTOR SCORE PGS (ANGST 2016)
-#E4_ANXCC_ANGST16              EA ANXIETY CASE CONTROL PGS (ANGST 2016)
-
-
-########################### 
-
-#heart failure 
-HRS_2008_data_with_PGS$heartfailure2yrs_bin
-
-
-########################### 
-
-#heart attack 
-HRS_2008_data_with_PGS$HRS2008_heartattack2yrs_bin
-
-# EA Myocardial Infartcion (MI) PGS (CARDIOGRAM 2015)
-HRS_2008_data_with_PGS$E4_MI_CARDIOGRAM15
-
-########################### 
-
-# hypertension 
-HRS_2008_data_with_PGS$hypertension_new_bin
-
-#E4_HTN_COGENT17
-# European Ancestry: Hypertension. PGSs for Hypertension were created using
-# results from a 2017 study conducted by the Continental Origins and Genetic
-# Epidemiology Network (COGENT) consortium.
-
-HRS_2008_data_with_PGS$E4_HTN_COGENT17
-
-########################### 
-
-#add smoking, alchohol consumption, BMI PGS and weight discrimination 
-
-HRS_2008_data_with_PGS$vigarious_physical_activity
-
-#add smoking, alchohol consumption, BMI PGS
-#add smoking, alchohol consumption, BMI PGS 
-#add smoking, alchohol consumption, BMI PGS 
-#add smoking, alchohol consumption, BMI PGS 
-#add smoking, alchohol consumption, BMI PGS 
-#add smoking, alchohol consumption, BMI PGS 
-#add smoking, alchohol consumption, BMI PGS 
-
-#We also have PTSD PGS 
-#We also have Alzheimer's PGS 
-#We have kidney disease PGS 
-
-nrow(HRS_2008_data_with_PGS)
-
-summary_stat = summary(HRS_2008_data_with_PGS$wealth_noIRA)
-summary_stat[2]
-summary_stat[3]
-summary_stat[5]
-
-first_quantile_value = summary_stat[6]/4
-second_quantile_value = summary_stat[6]/4 + summary_stat[6]/4
-third_quantile_value = summary_stat[6]/4 + summary_stat[6]/4 + summary_stat[6]/4
-
-subset_poorest = subset(HRS_2008_data_with_PGS, HRS_2008_data_with_PGS$wealth_noIRA<first_quantile_value)
-nrow(subset_poorest)
-
-subset_1 = subset(HRS_2008_data_with_PGS, HRS_2008_data_with_PGS$wealth_noIRA<summary_stat[2])
-nrow(subset_1)
-
-subset_2 = subset(HRS_2008_data_with_PGS, HRS_2008_data_with_PGS$wealth_noIRA>summary_stat[2] & HRS_2008_data_with_PGS$wealth_noIRA<summary_stat[3])
-nrow(subset_2)
-
-subset_3 = subset(HRS_2008_data_with_PGS, HRS_2008_data_with_PGS$wealth_noIRA>summary_stat[3] & HRS_2008_data_with_PGS$wealth_noIRA<summary_stat[5])
-nrow(subset_3)
-
-subset_4 = subset(HRS_2008_data_with_PGS, HRS_2008_data_with_PGS$wealth_noIRA>summary_stat[5])
-nrow(subset_4)
---
+++ renaming_HRS2018.R
@@ -0,0 +1,112 @@
+
+library(dplyr)
+
+
+
+
+
+#need to add hypertension_bin to 2018 
+#need to add alcohol_days_week to 2008 
+#add anxiety to all years 
+#add depression new bin to all years (NEW)
+#need to add CVD to all years 
+#check smoking 
+#add PTSD for all years  
+#add Alzheimer's for all years   
+#add kidney disease for all years    
+#limiting longstanding condition (recode to bin)
+
+
+
+
+directory = "/Users/aliyaamirova/Documents/KCL_postDoc/"
+
+SOURCE_ROOT = "Data_analysis/"
+OUTPUT_ROOT = "Data_analysis/"
+
+
+HRS_2018_data =  read.csv(paste(directory, SOURCE_ROOT, "HRS_2018_data/HRS2018_dataset_latest.csv", sep=""))
+
+HRS_2018 = data.frame(HRS_2018_data$HHIDPN)
+
+
+HRS_2018$HRS2018_alcohol_days_week = HRS_2018_data$alcohol_days_week
+HRS_2018$HRS2018_LGB_2016 = HRS_2018_data$LGB_2016
+HRS_2018$HRS2018_marital_status = HRS_2018_data$marital_status2018
+HRS_2018$HRS2018_married_bin = HRS_2018_data$married2018_bin
+HRS_2018$HRS2018_national_origin_ousideUS = HRS_2018_data$national_origin_ousideUS
+HRS_2018$HRS2018_separated_bin = HRS_2018_data$separated2018_bin
+HRS_2018$HRS2018_sexual_orientation = HRS_2018_data$sexual_orientation2018
+HRS_2018$HRS2018_smokes_ever = HRS_2018_data$smokes_ever
+HRS_2018$HRS2018_smokes_ever_bin = HRS_2018_data$smokes_ever_bin
+HRS_2018$HRS2018_smokes_now = HRS_2018_data$smokes_now
+HRS_2018$HRS2018_smokes_now_bin = HRS_2018_data$mokes_now_bin
+HRS_2018$HRS2018_start = HRS_2018_data$start
+HRS_2018$HRS2018_stop = HRS_2018_data$stop
+HRS_2018$HRS2018_Straight_2016 = HRS_2018_data$Straight_2016
+HRS_2018$HRS2018_summary_mean_score_discrim = HRS_2018_data$summary_mean_score_discrim
+HRS_2018$HRS2018_summary_mean_score_discrim_bin = HRS_2018_data$summary_mean_score_discrim_bin 
+HRS_2018$HRS2018_vigarious_physical_activity = HRS_2018_data$vigarious_physical_activity
+HRS_2018$HRS2018_wealth_noIRA = HRS_2018_data$wealth_noIRA
+HRS_2018$HRS2018_widowed_bin = HRS_2018_data$widowed2018_bin
+HRS_2018$HRS2018_yearsof_education = HRS_2018_data$yearsof_education2018
+HRS_2018$HRS2018_diabetes_ever = HRS_2018_data$diabetes_ever
+HRS_2018$HRS2018_diabetes_new = HRS_2018_data$diabetes_new
+HRS_2018$HRS2018_continious_age = HRS_2018_data$continious_age
+HRS_2018$HRS2018_angina_new_bin = HRS_2018_data$angina_new_bin
+HRS_2018$HRS2018_number_reasons_discrimination = HRS_2018_data$number_reasons_discrimination
+HRS_2018$HRS2018_race_white = HRS_2018_data$race_white
+HRS_2018$HRS2018_religion_bin = HRS_2018_data$
+HRS_2018$HRS2018_sex_1_0 = HRS_2018_data$sex_1_0_2018
+HRS_2018$HRS2018_sex_1_2 = HRS_2018_data$sex_1_2_2018
+HRS_2018$HRS2018_annual_income_self_employment = HRS_2018_data$annual_income_self_employment2018
+HRS_2018$HRS2018_never_married_bin = HRS_2018_data$never_married2018_bin
+HRS_2018$HRS2018_angina_ever = HRS_2018_data$HRS2018_angina_ever
+HRS_2018$HRS2018_angina_new = HRS_2018_data$HRS2018_angina_new
+HRS_2018$HRS2018_angina2yrs_bin = HRS_2018_data$HRS2018_angina2yrs_bin
+HRS_2018$HRS2018_BMI = HRS_2018_data$HRS2018_BMI
+HRS_2018$HRS2018_BMI_cat = HRS_2018_data$HRS2018_BMI_cat
+HRS_2018$HRS2018_BMI_category = HRS_2018_data$HRS2018_BMI_category
+HRS_2018$HRS2018_checklist_depression_bin = HRS_2018_data$HRS2018_checklist_depression_bin
+HRS_2018$HRS2018_discrim_afraidothers = HRS_2018_data$HRS2018_discrim_afraidothers
+HRS_2018$HRS2018_discrim_afraidothers_bin = HRS_2018_data$HRS2018_discrim_afraidothers_bin 
+HRS_2018$HRS2018_discrim_harassed = HRS_2018_data$HRS2018_discrim_harassed
+HRS_2018$HRS2018_discrim_harassed_bin = HRS_2018_data$HRS2018_discrim_harassed_bin
+HRS_2018$HRS2018_discrim_lessrespect = HRS_2018_data$HRS2018_discrim_lessrespect
+HRS_2018$HRS2018_discrim_lessrespect_bin = HRS_2018_data$HRS2018_discrim_lessrespect_bin
+HRS_2018$HRS2018_discrim_medical = HRS_2018_data$HRS2018_discrim_medical
+HRS_2018$HRS2018_discrim_medical_bin = HRS_2018_data$HRS2018_discrim_medical_bin
+HRS_2018$HRS2018_discrim_notclever = HRS_2018_data$HRS2018_discrim_notclever
+HRS_2018$HRS2018_discrim_notclever_bin = HRS_2018_data$HRS2018_discrim_notclever_bin
+HRS_2018$HRS2018_discrim_poorerservice = HRS_2018_data$HRS2018_discrim_poorerservice
+HRS_2018$HRS2018_discrim_poorerservice_bin = HRS_2018_data$HRS2018_discrim_poorerservice_bin
+HRS_2018$HRS2018_height_feet = HRS_2018_data$HRS2018_height_feet
+HRS_2018$HRS2018_height_inches = HRS_2018_data$HRS2018_height_inches
+HRS_2018$HRS2018_height_meters = HRS_2018_data$HRS2018_height_meters
+HRS_2018$HRS2018_limiting_condition_bin = HRS_2018_data$HRS2018_limiting_condition_bin
+HRS_2018$HRS2018_normalweight_bin = HRS_2018_data$HRS2018_normalweight_bin
+HRS_2018$HRS2018_overweight_bin = HRS_2018_data$HRS2018_overweight_bin
+HRS_2018$HRS2018_race = HRS_2018_data$HRS2018_race
+HRS_2018$HRS2018_race_black = HRS_2018_data$HRS2018_race_black
+HRS_2018$HRS2018_race_hispanic_latino = HRS_2018_data$HRS2018_race_hispanic_latino
+HRS_2018$HRS2018_race_nonwhite = HRS_2018_data$HRS2018_race_nonwhite
+HRS_2018$HRS2018_race_white = HRS_2018_data$HRS2018_race_white
+HRS_2018$HRS2018_reason_discrim1 = HRS_2018_data$HRS2018_reason_discrim1
+HRS_2018$HRS2018_reason_discrim1_reason_age = HRS_2018_data$HRS2018_reason_discrim1_reason_age
+HRS_2018$HRS2018_reason_discrim1_reason_disability = HRS_2018_data$HRS2018_reason_discrim1_reason_disability
+HRS_2018$HRS2018_reason_discrim1_reason_financial = HRS_2018_data$HRS2018_reason_discrim1_reason_financial
+HRS_2018$HRS2018_reason_discrim1_reason_gender = HRS_2018_data$HRS2018_reason_discrim1_reason_gender
+HRS_2018$HRS2018_reason_discrim1_reason_national = HRS_2018_data$HRS2018_reason_discrim1_reason_national
+HRS_2018$HRS2018_reason_discrim1_reason_otherreason = HRS_2018_data$HRS2018_reason_discrim1_reason_otherreason
+HRS_2018$HRS2018_reason_discrim1_reason_race = HRS_2018_data$HRS2018_reason_discrim1_reason_race
+HRS_2018$HRS2018_reason_discrim1_reason_religion = HRS_2018_data$HRS2018_reason_discrim1_reason_religion
+HRS_2018$HRS2018_reason_discrim1_reason_sexuality = HRS_2018_data$HRS2018_reason_discrim1_reason_sexuality
+HRS_2018$HRS2018_reason_discrim1_reason_weight = HRS_2018_data$HRS2018_reason_discrim1_reason_weight
+HRS_2018$HRS2018_underweight_bin = HRS_2018_data$HRS2018_underweight_bin
+HRS_2018$HRS2018_weight_kg = HRS_2018_data$HRS2018_weight_kg
+HRS_2018$HRS2018_weight_pounds = HRS_2018_data$HRS2018_weight_pounds
+HRS_2018$HRS2018_obese_bin = HRS_2018_data$HRS2018obese_bin
+
+write.csv(HRS_2018, file = paste(directory, SOURCE_ROOT, "HRS_2018_data/HRS2018_dataset_latest_renamed_vars.csv", sep=""))
+
+ls(HRS_2018)
