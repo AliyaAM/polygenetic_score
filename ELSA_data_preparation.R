c241a9b1e411fec6cf50b6476caa2801f2493a12
AliyaAM <aliya.amirova005@gmail.com>
2022-08-12 13:26:53 +0100

    Create ELSA_data_preparation.R

---
+++ ELSA_data_preparation.R
@@ -0,0 +1,82 @@
+library(dplyr)
+library(tidyr)
+library(stats)
+
+library(survival)
+library(survminer)
+
+
+
+
+
+#need to add hypertension_bin to 2018 
+#need to add alcohol_days_week to 2008 
+#add anxiety to all years 
+#add depression new bin to all years (NEW)
+#need to add CVD to all years 
+#check smoking 
+#add PTSD for all years  
+#add Alzheimer's for all years   
+#add kidney disease for all years    
+#limiting longstanding condition (recode to bin)
+
+
+
+
+directory = "/Users/aliya/my_docs"
+
+#"/Users/aliyaamirova/"
+
+DATA_ROOT = "/KCL_postDoc/Data_analysis/"
+SOURCE_ROOT = "/proj/Cumulative_effects_HRS/Version_2_analysis/"
+
+source((paste(directory, SOURCE_ROOT, "sort_timepoints.R", sep="")))
+
+#/Users/aliya/my_docs/KCL_postDoc/Data_analysis/HRS_2008_data
+#/Users/aliya/my_docs/proj/Cumulative_effects_HRS/Version_2_analysis
+
+###########
+###########
+
+ELSA_data = read.csv(paste(directory, DATA_ROOT, "DATA_ELSA/ELSAdiscrimination_data_wave5.csv", sep = ""))
+#/Users/aliya/my_docs/KCL_postDoc/Data_analysis/DATA_ELSA
+
+#HRS polygenic scores data: 
+polygenic_scores_data = read.csv(paste(directory, DATA_ROOT, "/HRS_polygenetic_scores_biomarkers/pgenscore4e_r.csv", sep = ""))
+
+#polygenic_scores_data = read.csv(paste(directory, DATA_ROOT, "HRS_2012_data/pgenscore4e_r.csv", sep = ""))
+
+#ELSA polygenic scores data: 
+polygenic_scores_ELSA_data = read.csv(paste(directory, DATA_ROOT, "DATA_ELSA/polygenic_scores_ELSA_data_standardized.csv", sep = "")) 
+
+#polygenic_scores_ELSA_data = read.csv(paste(directory, DATA_ROOT, "DATA_ELSA/polygenic_scores_ELSA_data_standardized.csv", sep = "")) 
+
+#subset polygenic scores data to those who provided data for the main survey 
+ID_ELSA = unique(ELSA_data$idauniq)
+ELSA_data_polygenic_scores = polygenic_scores_ELSA_data[ELSA_data$idauniq %in% ID_ELSA,]
+
+#subset main ELSA data to thos ewho provided PGS data 
+ID_ELSA_PGS_unique =unique(polygenic_scores_data$idauniq)
+#ELSA_data_PGS_subset = ELSA_data[polygenic_scores_data$idauniq %in% ID_ELSA_PGS_unique, ]
+
+#nrow(ELSA_data_PGS_subset)
+#nrow(ELSA_data_polygenic_scores)
+
+#join PGS scores with ELSA main dataset 
+ELSA_data_with_PGS = bind_cols(ELSA_data, ELSA_data_polygenic_scores) 
+
+#ELSA_data_with_PGS$age (check min age is above 50 in ELSA)
+print(min(ELSA_data_with_PGS$w8age, na.rm = TRUE))
+
+#subset the data to those aged 50 and over: 
+ELSA_data_with_PGS = subset(ELSA_data_with_PGS, 
+                            ELSA_data_with_PGS$w1age >=50 & 
+                              ELSA_data_with_PGS$w2age >=50 & 
+                              ELSA_data_with_PGS$w3age >=50 & 
+                              ELSA_data_with_PGS$w4age >=50 & 
+                              ELSA_data_with_PGS$w5age >=50 & 
+                              ELSA_data_with_PGS$w6age >=50 & 
+                              ELSA_data_with_PGS$w7age >=50 & 
+                              ELSA_data_with_PGS$w8age >=50) 
+
+write.csv(ELSA_data_with_PGS, file =  paste(directory, DATA_ROOT, "DATA_ELSA/ELSA_data_with_PGS.csv", sep = "")) 
