4e2caf2f8a2f50628d879c2ee7072fb8dd1d4605
AliyaAM <aliya.amirova005@gmail.com>
2022-08-12 13:27:07 +0100

    Update adding_polygenetic_scores.R

--- adding_polygenetic_scores.R
+++ adding_polygenetic_scores.R
@@ -37,49 +37,12 @@ source((paste(directory, SOURCE_ROOT, "sort_timepoints.R", sep="")))
 
 ###########
 ###########
-
-ELSA_data = read.csv(paste(directory, DATA_ROOT, "DATA_ELSA/ELSAdiscrimination_data_wave5.csv", sep = ""))
 #/Users/aliya/my_docs/KCL_postDoc/Data_analysis/DATA_ELSA
 
 #HRS polygenic scores data: 
 polygenic_scores_data = read.csv(paste(directory, DATA_ROOT, "/HRS_polygenetic_scores_biomarkers/pgenscore4e_r.csv", sep = ""))
 
-#polygenic_scores_data = read.csv(paste(directory, DATA_ROOT, "HRS_2012_data/pgenscore4e_r.csv", sep = ""))
 
-#ELSA polygenic scores data: 
-polygenic_scores_ELSA_data = read.csv(paste(directory, DATA_ROOT, "DATA_ELSA/polygenic_scores_ELSA_data_standardized.csv", sep = "")) 
-
-#polygenic_scores_ELSA_data = read.csv(paste(directory, DATA_ROOT, "DATA_ELSA/polygenic_scores_ELSA_data_standardized.csv", sep = "")) 
-
-#subset polygenic scores data to those who provided data for the main survey 
-ID_ELSA = unique(ELSA_data$idauniq)
-ELSA_data_polygenic_scores = polygenic_scores_ELSA_data[ELSA_data$idauniq %in% ID_ELSA,]
-
-#subset main ELSA data to thos ewho provided PGS data 
-ID_ELSA_PGS_unique =unique(polygenic_scores_data$idauniq)
-#ELSA_data_PGS_subset = ELSA_data[polygenic_scores_data$idauniq %in% ID_ELSA_PGS_unique, ]
-
-nrow(ELSA_data_PGS_subset)
-nrow(ELSA_data_polygenic_scores)
-
-#join PGS scores with ELSA main dataset 
-ELSA_data_with_PGS = bind_cols(ELSA_data, ELSA_data_polygenic_scores) 
-
-#ELSA_data_with_PGS$age (check min age is above 50 in ELSA)
-print(min(ELSA_data_with_PGS$w8age, na.rm = TRUE))
-
-#subset the data to those aged 50 and over: 
-ELSA_data_with_PGS = subset(ELSA_data_with_PGS, 
-                            ELSA_data_with_PGS$w1age >=50 & 
-                              ELSA_data_with_PGS$w2age >=50 & 
-                              ELSA_data_with_PGS$w3age >=50 & 
-                              ELSA_data_with_PGS$w4age >=50 & 
-                              ELSA_data_with_PGS$w5age >=50 & 
-                              ELSA_data_with_PGS$w6age >=50 & 
-                              ELSA_data_with_PGS$w7age >=50 & 
-                              ELSA_data_with_PGS$w8age >=50) 
-
-write.csv(ELSA_data_with_PGS, file =  paste(directory, DATA_ROOT, "DATA_ELSA/ELSA_data_with_PGS.csv", sep = "")) 
 
 #############
 
